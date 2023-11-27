6f598d38979026165b2d69ce0b4a1b6978f60d5b
CriticPat <151105228+CriticPat@users.noreply.github.com>
2023-11-26 22:00:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,35 @@ HEIGHT =800
 screen =pygame.display.set_mode([WIDTH,HEIGHT])
 
 #Assets that will be used for the game
-gun=[]
-bg=pygame.image.load('assets/bg/bg.jpg')
+guns=pygame.transform.scale(pygame.image.load('assets/gun/gun.png'),(100, 100))
+bg=pygame.image.load('assets/bg/bg.png')
 targets=[]
 level=1
-
+ 
+def draw_gun():
+	mouse_pos=pygame.mouse.get_pos()
+	gun_point=(WIDTH/2, HEIGHT-200)
+	lasers=['brown']
+	clicks=pygame.mouse.get_pressed()
+	if mouse_pos[0] != gun_point[0]:
+		slope=(mouse_pos[1]-gun_point[1])/(mouse_pos[0]-gun_point[0])
+	else:
+		slope=-100000000000000
+	angle = math.atan(slope)
+	rotation=math.degrees(angle)
+	if mouse_pos[0]	< WIDTH/2:
+		gun= pygame.transform.flip(guns,True, False)
+		if mouse_pos[1] < 600:
+			screen.blit(pygame.transform.rotate(gun,90-rotation), ((WIDTH/2)-90,HEIGHT-250))
+			if clicks[0]:
+				pygame.draw.circle(screen,lasers[level-1], mouse_pos,5)
+	else:
+		gun=guns
+		if mouse_pos[1] < 600:
+			screen.blit(pygame.transform.rotate(gun,270-rotation), (WIDTH/2-30,HEIGHT-250))
+			if clicks[0]:
+				pygame.draw.circle(screen,lasers[level-1], mouse_pos,5)
+			
 #Getting the game to run
 run=True
 
@@ -26,12 +50,15 @@ while run:
 	screen.fill('white')
 	screen.blit(bg,(0,0))
 	
+	if level >0:
+		draw_gun()
+	
 	#This willl close the program when the x on window is presssed
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
 			run=False
 			
-	#This dislpay what the while asks		
+	#This dislpayss what the while asks		
 	pygame.display.flip()
 	
 pygame.quit()#Will close the program
