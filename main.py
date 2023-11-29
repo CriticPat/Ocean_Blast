4e2c2c91f33417cc047d635c31297855e6642ee6
CriticPat <patacu20@cjc.edu.bz>
2023-11-29 01:01:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,34 @@ pygame.init()
 # Game Window Setup
 fps = 60
 timer = pygame.time.Clock()
-font = pygame.font.Font('assets/Fonts/MelodyStories.otf', 32)
+font = pygame.font.Font('assets/Fonts/MelodyStories.otf', 60)
 WIDTH, HEIGHT = 900, 700
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 target_images=[]
+menu=True
+high_score=0
 
 # Load Assets
 guns = pygame.transform.scale(pygame.image.load('assets/gun/Harpoon.png'), (150, 150))
 bg = pygame.transform.scale(pygame.image.load('assets/bg/bg.jpg'), (900, 800))
+banner= pygame.transform.scale(pygame.image.load(f'assets/banner/banner.png'), (900,100))
 heart_image = pygame.transform.scale(pygame.image.load('assets/life/heart.png'), (30, 30))
+menu_image= pygame.image.load(f'assets/menu/menu.png')
 for i in range(1,5):
      target_images.append(pygame.image.load(f'assets/targets/{i}.png'))
 
+def draw_menu():
+    screen.blit(menu_image,(0,0))
+    mouse_pos=pygame.mouse.get_pos()
+    clicks=pygame.mouse.get_pressed()
+    play_button=pygame.rect.Rect((302, 330),(245,75))
+    screen.blit(font.render(f'{high_score}',True,'black'),(510, 198))
+
+    if play_button.collidepoint(mouse_pos) and clicks[0]and not clicked: 
+        level=1
+        menu=False
+                                                      
+    
 # Game Variables
 lives = 3  # Starting lives
 level = 1
@@ -58,33 +74,6 @@ def draw_lives():
     for i in range(lives):
         screen.blit(heart_image, (10 + i * 40, 10))
 
-class Enemy:
-    def __init__(self, x, y, type, speed):
-        self.x = x
-        self.y = y
-        self.type = type
-        self.speed = speed
-
-        # Add other attributes like image, health, etc.
-
-    def draw(self):
-        # Placeholder for drawing the enemy
-        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 20)
-
-    def update(self):
-        # Placeholder for updating enemy position
-        self.y += self.speed
-        if self.y > HEIGHT:
-            global lives
-            lives -= 1  # Lose a life if enemy passes the bottom
-            targets.remove(self)
-
-# Function to spawn enemies
-def spawn_enemy():
-    # Placeholder enemy spawning logic
-    if random.randint(0, 60) == 1:  # Random chance to spawn an enemy
-        new_enemy = Enemy(random.randint(0, WIDTH), 0, 1, 5)
-        targets.append(new_enemy)
 
 # Main Game Loop
 run = True
@@ -92,12 +81,11 @@ while run:
     timer.tick(fps)
     screen.fill('white')
     screen.blit(bg, (0, 0))
+    screen.blit(banner,(0,HEIGHT-100))
 
-    # Spawn and update enemies
-    spawn_enemy()
-    for enemy in targets[:]:
-        enemy.update()
-        enemy.draw()
+    if menu:
+        level==0
+        draw_menu()
 
     draw_gun()
     draw_lives()
